3e73a354cf256be4ea107f240a639e58963c083d
Azzni96 <nihadazzam96@gmail.com>
2024-10-03 10:46:47 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'vite';
+import {VitePWA} from 'vite-plugin-pwa';
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ttf}']
+      }
+    }),
+  ],
+});
