26947dd567aae0a5f19dcccbd1c647da653d4021
Azzni96 <nihadazzam96@gmail.com>
2024-10-06 00:14:42 +0300

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0hu47a1d93g"
+    "revision": "0.qat0ttacp0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
